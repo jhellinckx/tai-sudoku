7c60189c90c5a29693b8a440e195069b4e39a2a0
Jérôme Hellinckx <jerome.hellinckx@famoco.com>
2019-05-13 02:33:29 +0200

    [add] save model and show some ocr predictions

--- sudoku.py
+++ sudoku.py
@@ -2,6 +2,7 @@ import sys
 import os
 import random
 from operator import itemgetter, attrgetter
+from math import ceil
 
 import matplotlib.image as mpli
 import matplotlib.pyplot as plt
@@ -9,9 +10,12 @@ import numpy as np
 import cv2
 
 from keras.models import Sequential
+from keras.models import load_model
 from keras.layers import Dense
 from keras.utils import plot_model
 from keras.utils import np_utils
+
+random.seed(1)
 
 img_rows = 28
 img_cols = 28
@@ -20,6 +24,8 @@ img_mode = cv2.IMREAD_GRAYSCALE
 input_dim = img_rows * img_cols
 num_classes = 9
 validation_set_proportion = 0.2
+model_name = '25x9'
+model_filename = f'{model_name}.h5'
 
 def build_ocr_model():
     model = Sequential()
@@ -33,10 +39,27 @@ def build_ocr_model():
 def train_ocr_model():
     X_train, y_train, X_val, y_val = get_char74k_dataset()
     model = build_ocr_model()
-    model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=100, batch_size=200, verbose=1)
-    model_name = '25x9'
-    model.save(f'{model_name}.h5')
+    model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=50, batch_size=200, verbose=1)
+    model.save(model_filename)
 
+def show_random_predictions(num_predictions=20):
+    X_train, y_train, X_val, y_val = get_char74k_dataset()
+    model = load_model(model_filename)
+    X_pred = X_val[:num_predictions]
+    preds = model.predict(X_pred, batch_size=num_predictions)
+    plt.figure(figsize=(10, 10))
+    columns = 5
+    rows = ceil(num_predictions / columns)
+    for i in range(num_predictions):
+        pred = preds[i]
+        img = X_pred[i]
+        label = np.argmax(pred) + 1
+        confidence = f'{round(pred[label - 1] * 100, 2)}'
+        img *= 255 # Rescale to 0-255 range
+        plt.subplot(rows, columns, i + 1)
+        plt.title(f'This is a {label} ({confidence}%)', fontsize=10)
+        plt.imshow(img.reshape(img_rows, img_cols), cmap=plt.cm.gray)
+    plt.show()
 
 def show_processed_image(img_array):
     img_array *= 255
@@ -66,15 +89,15 @@ def get_char74k_dataset(dataset_dir='res/ocr'):
         val_data += class_data[:slice_index]
         train_data += class_data[slice_index:]
     random.shuffle(train_data)
+    random.shuffle(val_data)
     X_train, y_train = read_images(train_data)
-
     X_val, y_val = read_images(val_data)
-    
+    #show_processed_image(X_train[0])
     return (X_train, y_train, X_val, y_val)
-    
 
 if __name__ == '__main__':
-    train_ocr_model()
+    show_random_predictions()
+    
 
 
 
